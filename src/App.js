6cdb093a870e4bca92ebc5b781eb8e595dae73fa
Rajappan043 <124550957+Rajappan043@users.noreply.github.com>
2024-01-30 10:38:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import LoginPage from "./Components/Login/Login";
 import "./App.css";
-//import SignUp from "./Components/SignUp/Signup";
 function App() {
   return (
     <>
       <LoginPage></LoginPage>
-      {/* <SignUp></SignUp> */}
     </>
   );
 }
